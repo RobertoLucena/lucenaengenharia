9241c18b7216bf89ea6b3a5056e723850e354089
Roberto Lucena <roberto.nlucena@gmail.com>
2021-03-16 17:58:48 -0300

    atualizando botões

---
+++ code.js
@@ -0,0 +1,15 @@
+function telefone() {
+    if (document.getElementById("telefone").innerHTML == "(92) 99155-9582") {
+        document.getElementById("telefone").innerHTML = "";
+    } else {
+        document.getElementById("telefone").innerHTML = "(92) 99155-9582"
+    }
+}
+
+function email() {
+    if (document.getElementById("email").innerHTML == "robertolucena.eng@gmail.com") {
+        document.getElementById("email").innerHTML = "";
+    } else {
+        document.getElementById("email").innerHTML = "robertolucena.eng@gmail.com"
+    }
+}
